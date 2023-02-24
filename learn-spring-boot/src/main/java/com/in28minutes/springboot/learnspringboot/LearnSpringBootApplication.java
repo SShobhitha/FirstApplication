41bfd38cdc561d374b80de5249d79247c963d8fa
shobhitha <shobhithagowda.1897@gmail.com>
2023-02-24 10:50:26 +0530

    second change

--- learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
+		int i=0;
 		SpringApplication.run(LearnSpringBootApplication.class, args);
 	}
 
